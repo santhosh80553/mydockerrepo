83ae24663b8a9695f3f13bc01cad0903f2715f50
santhosh80553 <107468516+santhosh80553@users.noreply.github.com>
2023-09-25 14:25:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,12 @@ images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
 ]
+
 # Duplicated function
 def duplicate_index():
     url = random.choice(images)
     return render_template('index.html', url=url)
+    
 @app.route('/')
 def index():
     url = random.choice(images)
